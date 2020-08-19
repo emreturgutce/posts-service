f4e1209685201623fd816b91345dc05a9c528028
emreturgutce <emrebursalee@gmail.com>
2020-08-19 14:05:35 +0300

    basic posts route implemented

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(express.json());
+
+const posts = {};
+
+app.get('/posts', (req, res) => {
+  res.json(posts);
+});
+
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+
+  posts[id] = { id, title };
+
+  res.status(201).json(posts[id]);
+});
+
+app.listen(4000, () => console.log('App is running on port 4000'));
